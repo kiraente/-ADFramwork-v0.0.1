8c964ec684b8c05871d9023f49706c358d92a754
kiraente <finae@shaw.ca>
2015-05-13 21:57:10 -0600

    item database item

---
+++ Assets/Scripts/Items/Database/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using ADFramework.ItemCollections;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class Item  {
+    public int _Qty;
+    public int _StackingQty;
+    public int _SellingPrice;
+    public int _BuyingPrice;
+
+    public string _Name;
+    public string _Desc;
+
+    public Dictionary<enumADFWItemAttributes, bool> _ItemAttributes;
+    public Dictionary<enumADFWItemType, bool> _ItemType;
+}
