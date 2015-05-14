01c93a2c4312ae182f1b6f0ed0c3e8b7160b6c7b
kiraente <finae@shaw.ca>
2015-05-13 22:17:34 -0600

    UPDATE 5/13

---
+++ Assets/Scripts/Items/Database/ItemDatabase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using ADFramework.ItemCollections;
+public class ItemDatabase : MonoBehaviour {
+    protected List<ADFWItem> _Items = new List<ADFWItem>();
+
+    public ItemDatabase()
+    {
+            //create item
+        _Items.AddRange(new ADFramework.ItemCollections.UseableItemCollections.Potions.ADFWStarterHealthPotion[] {
+                new ADFramework.ItemCollections.UseableItemCollections.Potions.ADFWStarterHealthPotion(10,10,99),
+                new ADFramework.ItemCollections.UseableItemCollections.Potions.ADFWStarterHealthPotion(20,20,99),
+                new ADFramework.ItemCollections.UseableItemCollections.Potions.ADFWStarterHealthPotion(40,40,50)
+        });
+    }
+    public ADFWItem ItemAt(int idx)
+    {
+        if (idx < _Items.Count && idx >= 0)
+            return _Items[idx];
+        else
+            return null;
+    }    
+
+}
